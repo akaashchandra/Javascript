7957428e55c89f1cc0a5cfcc3baf29a93613ff64
Kundapur <Akaash.Kundapur@unisys.com>
2018-09-23 10:32:35 +0530

    day 2

---
+++ allscripts/sample1.js
@@ -0,0 +1,23 @@
+// document.write("Welcome to Javascript")
+var a=10;
+var b
+console.log(b)
+console.log(a)
+// var ans = confirm("Are you sure you want to continue?");
+// console.log("th Answer i "+ ans)
+// if(ans==true){
+//     var myName = localStorage.getItem('myName');
+//     if(myName==null){
+//         localStorage.setItem('myName',"Akash")
+//     }
+//     if(b==undefined){
+//         b=100;
+//     }
+//     document.getElementById('myHeading').innerHTML="Welcome to Javascript";
+// }else{
+//     alert("Ok Bye")
+// }
+
+var userName= prompt("PLease Enter your name ?")
+
+alert("Hey "+userName)
